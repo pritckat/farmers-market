57026a0104b1ee5868e08efbc689460e6492384a
pritckat <pritckat@gmail.com>
2021-05-12 10:58:47 -0400

    app connects to localhost and db

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
-const MongoStore = require('connect-mongo')('session')
-const flash = require('express-flash')
-const logger = require('morgan')
-connectDB = require('./config/database')
+const MongoStore = require('connect-mongo')(session)
+const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
-
+const flash = require('connect-flash')
 
 require('dotenv').config({path: './config/.env'})
 
@@ -18,19 +17,23 @@ connectDB()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(logger('dev'))
+app.use(flash())
+app.set('views', path.join(__dirname, '/views/pages'));
+
+//app.use(logger('dev'))
 
 app.use(
     session({
-        secret: 'keyboard cat',
-        resave: false,
-        saveUninitialized: false,
-        store: new MongoStore({ mongooseConnection: mongoose.connection})
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
     })
-)
+  )
 
-app.use(passposrt.initialize())
+app.use(passport.initialize())
 app.use(passport.session())
 
 app.use(flash())
