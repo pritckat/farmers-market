ac75a390ff4b1b7ec2baecd3ff2c412fa0a591d2
pritckat <pritckat@gmail.com>
2021-06-02 12:41:18 -0400

    add getItems method to item controller

--- controllers/item.js
+++ controllers/item.js
@@ -1,5 +1,10 @@
+const Item = require('../models/item')
+
 module.exports = {
     getItem: (req,res) => {
         res.render('item.ejs')
+    },
+    getItems: (req,res) => {
+        res.render('items.ejs')
     },
 }
