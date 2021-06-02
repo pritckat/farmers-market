1d1cf17905e84a409d8a058f2a4fb78336164ed6
pritckat <pritckat@gmail.com>
2021-06-02 11:40:05 -0400

    add getStore and createStore methods to store controller

--- controllers/store.js
+++ controllers/store.js
@@ -1,5 +1,27 @@
+const { Store } = require("express-session")
+
 module.exports = {
     getStores: (req,res) => {
         res.render('stores.ejs')
+    },
+    createStore: (req,res) => {
+        try{
+            await Store.create({
+                name: req.body.name,
+                user: req.user._id,
+            })
+            console.log('Store created')
+            res.redirect('/store')
+        }catch(err){
+            console.log(err)
+        }
+    },
+    getStore: (req,res) => {
+        try {
+            const store = await Store.findOne({_id: req.params.id})
+            res.render('store', {store: store})
+        }catch(err){
+            console.log(err)
+        }
     },
 }
