e579277279d434912378ddb3ee616b1e42b13227
pritckat <pritckat@gmail.com>
2021-06-02 12:42:06 -0400

    add createItem to item controller

--- controllers/item.js
+++ controllers/item.js
@@ -7,4 +7,19 @@ module.exports = {
     getItems: (req,res) => {
         res.render('items.ejs')
     },
+    createItem: async (req,res) => {
+        try{
+            console.log(req.params)
+            const item = await Item.create({
+                name: req.body.name,
+                user: req.user._id,
+                store: req.params.id,
+                cost: req.body.cost
+            })
+            console.log('Item created')
+            res.redirect(`/stores/${req.params.id}`)
+        }catch(err){
+            console.log(err)
+        }
+    },
 }
